920b7f546f1f6fc35575d7a82e56a7dd3e6543e6
Subashchandrabose <subashlearner@gmail.com>
2022-04-26 22:56:47 +0530

    login change

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,62 +2,38 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div >
-            <div className="row p-5 bg-dark">
-                <div className="container login-form bg-light">
-                    <form>
-
-                        <div className="form-outline mb-4">
-                            <label className="form-label text-bold" for="form2Example1">Email address</label>
-                            <input type="email" id="form2Example1" className="form-control" placeholder='Email address' required/>
-
-                        </div>
-
-
-                        <div className="form-outline mb-4">
-                            <label className="form-label" for="form2Example2">Password</label>
-                            <input type="password" id="form2Example2" className="form-control" placeholder='Password' required/>
-
-                        </div>
-
-
-                        <div className="row mb-4">
-                            <div className="col d-flex justify-content-center">
-
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="form2Example31" />
-                                    <label className="form-check-label me-3" for="form2Example31"> Remember me </label>
-                                    <a href="#!">Forgot password?</a>
-                                </div>
-                            </div>
-
-
-
-                        </div>
-
-
-                        <div className="text-center">
-                            <button type="button" className="btn btn-outline-dark btn-block mb-4 px-5 py-2">Sign in</button>
-                        </div>
-
-
-                        <div className="text-center">
-                            <p>Not a member? Go ahead and Register</p>
-                            <p>or sign up with:</p>
-                            <button type="button" className="btn btn-link btn-dark btn-floating mx-1">
-                                <i className="fa fa-facebook-f text-light"></i>
-                            </button>
-
-                            <button type="button" className="btn btn-link btn-dark btn-floating mx-1">
-                                <i className="fa fa-google text-light"></i>
-                            </button>
-
-
-
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <>
+       <div className="container">
+    <div className="row justify-content-center mt-5">
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="card shadow">
+          <div className="card-title text-center border-bottom">
+            <h2 className="p-3">Login</h2>
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="mb-4">
+                <label for="username" className="form-label">Username/Email</label>
+                <input type="text" className="form-control" id="username" />
+              </div>
+              <div className="mb-4">
+                <label for="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" />
+              </div>
+              <div className="mb-4">
+                <input type="checkbox" className="form-check-input" id="remember" />
+                <label for="remember" className="form-label"> &nbsp; Remember Me</label>
+              </div>
+              <div className="d-grid">
+                <button type="submit" className="btn text-light main-bg buton">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
+    </div>
+  </div>
+      
+  </>
     )
 }
--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -2,54 +2,42 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>
-        <div className="row p-5 bg-dark">
-                <div className="container login-form bg-light">
-                    <form>
-
-                        <div className="form-outline mb-4">
-                            <label className="form-label text-bold" for="form2Example1">Email address</label>
-                            <input type="email" id="form2Example1" className="form-control" placeholder='Email address' required/>
-
-                        </div>
-
-
-
-                        <div className="form-outline mb-4">
-                            <label className="form-label" for="form2Example2">New Password</label>
-                            <input type="password" id="form2Example2" className="form-control" placeholder='New Password' required/>
-
-                        </div>
-
-                        
-                        <div className="form-outline mb-4">
-                            <label className="form-label" for="form2Example2">Re-enter Password</label>
-                            <input type="password" id="form2Example2" className="form-control" placeholder='Re-enter Password' required/>
-
-                        </div>
-
-
-                        <div className="row mb-4">
-                            <div className="col d-flex justify-content-center">
-
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="form2Example31" />
-                                    <label className="form-check-label me-3" for="form2Example31"> I've accepted all your terms and conditions. </label>
-            
-                                </div>
-                            </div>
-
-
-
-                        </div>
-
-
-                        <div className="text-center">
-                            <button type="button" className="btn btn-outline-dark btn-block mb-4 px-5 py-2"> Register </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-    </div>
+    <>
+    <div className="container">
+ <div className="row justify-content-center mt-5">
+   <div className="col-lg-4 col-md-6 col-sm-6">
+     <div className="card shadow">
+       <div className="card-title text-center border-bottom">
+         <h2 className="p-3">Register</h2>
+       </div>
+       <div className="card-body">
+         <form>
+           <div className="mb-4">
+             <label for="username" className="form-label">Username/Email</label>
+             <input type="text" className="form-control" id="username" />
+           </div>
+           <div className="mb-4">
+             <label for="password" className="form-label">Password</label>
+             <input type="password" className="form-control" id="password" />
+           </div>
+           <div className="mb-4">
+             <label for="password" className="form-label">Re-Password</label>
+             <input type="password" className="form-control" id="password" />
+           </div>
+           <div className="mb-4">
+             <input type="checkbox" className="form-check-input" id="remember" />
+             <label for="remember" className="form-label">&nbsp; I've accepted all your terms and conditions.</label>
+           </div>
+           <div className="d-grid">
+             <button type="submit" className="btn text-light main-bg buton">Sign up</button>
+           </div>
+         </form>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>
+   
+</>
   )
 }
